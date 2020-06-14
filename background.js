6c8cb849ef065b7847d43a4c27a155ffcbebaf96
Cyrille <cyrillebenoit>
2020-06-14 17:59:45 +0200

    Ported Bingosync+ to Chrome

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let config = localStorage.getItem("bsp_config");
 
 function updateTabs(message) {
@@ -60,4 +61,27 @@ browser.runtime.onMessage.addListener((message, sender, respond) => {
             console.log("what is this message lol", message.type);
             break;
     }
+=======
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                // That fires when a page's URL contains a 'g' ...
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 'bingosync.com/room' },
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+>>>>>>> Ported Bingosync+ to Chrome
 });
